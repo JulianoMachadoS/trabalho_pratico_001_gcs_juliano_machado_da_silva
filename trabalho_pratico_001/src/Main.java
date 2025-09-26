d02d6bc1ac26cd34c4018d8220dc948254589d1f
Juliano <mach4d0@proton.me>
2025-09-26 19:03:14 -0300

    menu de teste atualizado

--- trabalho_pratico_001/src/Main.java
+++ trabalho_pratico_001/src/Main.java
@@ -5,8 +5,25 @@ Matricula: 25108646-8 */
 public class Main {
     public static void main(String[] args) {
         EditorDetexto et = new EditorDetexto();
+        System.out.print("\f");
         String textoTeste = "   Palavra Para teste  ";
-        System.out.println("Texto original: " + textoTeste);
-        System.out.println(et.editorDeTexto(textoTeste));
+
+        boolean loop = false;
+        while (!loop == true){// inicio do menu de teste
+            System.out.println("Texto original: " + textoTeste);
+            System.out.println(et.editorDeTexto(textoTeste));
+            loop = parasistema();
+        }
+    }
+    public static boolean parasistema(){
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("\nDigite 1 para SAIR ou qualquer outra coisa para Reiniciar");
+        String sair = teclado.nextLine();
+        if (sair.indexOf("1") >= 0) {
+            System.out.println("FIM DO TESTE\n");
+            return true;
+        }
+        System.out.println("----------------- TESTE REINICIADO ---------------------");
+        return false;
     }
 }
