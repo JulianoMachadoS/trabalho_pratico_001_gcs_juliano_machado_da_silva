d449013459d0d081e5408915f83a736f3ec708a9
Juliano <mach4d0@proton.me>
2025-09-26 18:47:17 -0300

    V1.2 metodos de edição adicionados

--- trabalho_pratico_001/src/EditorDetexto.java
+++ trabalho_pratico_001/src/EditorDetexto.java
@@ -4,6 +4,8 @@ public class EditorDetexto {
     public String editorDeTexto(String texto){
         texto = texto.toLowerCase();
         texto = removeEspacosInicioFim(texto);
+        texto = removeSimbolosDoInico(texto);
+        texto = removeSimbuloDoFinal(texto);
         return texto;
     }
     public String removeEspacosInicioFim (String texto){
@@ -19,6 +21,54 @@ public class EditorDetexto {
                 texto = texto.substring(0,j);
             } else {
                 j = -1;
+            }
+        }
+        return texto;
+    }
+    public String removeSimbuloDoFinal (String texto){
+        for (int i = 0; i < texto.length(); i++){
+            if (i == texto.length()-1){
+                if ( ehSimbulo(texto.charAt(i)) && (i != '!' && i != '?' && i != '¡' && i != '¿' && i != '.')){
+                    texto = texto.substring(0,i);
+                    i--;
+                    i--;
+                }
+            }
+
+        }
+        return texto;
+    }
+    public boolean ehLetradoAlfabetoAZ(char letra){
+        letra = Character.toLowerCase(letra);
+        if (letra == 'a' || letra == 'b' || letra == 'c' || letra == 'd' || letra == 'e' || letra == 'f' ||
+                letra == 'g' || letra == 'h' || letra == 'i' || letra == 'j' || letra == 'k' || letra == 'l' ||
+                letra == 'm' || letra == 'n' || letra == 'o' || letra == 'p' || letra == 'q' || letra == 'r' ||
+                letra == 's' || letra == 't' || letra == 'u' || letra == 'v' || letra == 'w' || letra == 'x' ||
+                letra == 'y' || letra == 'z'){
+            return true;
+        }
+        return false;
+    }
+    public boolean ehNumero(char letra){
+        letra = Character.toLowerCase(letra);
+        if (letra == '1' || letra == '2' || letra == '3' || letra == '4' || letra == '5' ||
+                letra == '6' || letra == '7' || letra == '8' || letra == '9' || letra == '0' ){
+            return true;
+        }
+        return false;
+    }
+    public boolean ehSimbulo (char letra){
+        letra = Character.toLowerCase(letra);
+        if (ehLetradoAlfabetoAZ(letra) || letra == ' ' || ehNumero(letra)){
+            return false;
+        }
+        return true;
+    }
+    public String removeSimbolosDoInico(String texto){
+        for (int i =  0 ; i < texto.length(); i++) {
+            if (i == 0 && ehSimbulo(texto.charAt(i)) && i != '¿' && i != '¡'){
+                texto = texto.substring(i+1);
+                i--;
             }
         }
         return texto;
